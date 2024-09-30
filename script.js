e0534dc13866fd0fba60e666c3963d3f36a5e078
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-09-30 13:58:50 +0200

    added render functionality

--- script.js
+++ script.js
@@ -10,20 +10,25 @@ let url = "https://pokeapi.co/api/v2/";
 // getAllPokemonData();
 
 async function getOnePokemonData() {
-    let response = await fetch(url + "pokemon/1/");
-    let pokemonDataAsJson = await response.json();
-    console.log(pokemonDataAsJson);
-    renderPokemonEntries(pokemonDataAsJson);
+    for (let i = 1; i <= 20; i++) {
+        let response = await fetch(url + "pokemon/" + i);
+        let pokemonDataAsJson = await response.json();
+        console.log(pokemonDataAsJson);
+        renderPokemonEntries(pokemonDataAsJson, i);
+    }
 }
 
-// getOnePokemonData();
-
-function renderPokemonEntries(pokemonData) {
+function renderPokemonEntries(pokemonData, i) {
     let entryRef = document.getElementById('main-content');
-    entryRef.innerHTML = getPokemonEntriesTemplate(pokemonData);
+    entryRef.innerHTML += getPokemonEntriesTemplate(pokemonData, i);
+
+    let typesRef = document.getElementById('pokemon-types' + i);
+    for (let i = 0; i < pokemonData.types.length; i++) {
+        typesRef.innerHTML += getPokemonTypesTemplate(pokemonData, i);
+    }
 }
 
-function getPokemonEntriesTemplate() {
+function getPokemonEntriesTemplate(pokemonData, i) {
     return /*html*/`
         <div class="pokemon-entry">
             <div>
@@ -31,12 +36,18 @@ function getPokemonEntriesTemplate() {
                 <span>#${pokemonData.id}</span>
             </div>
             <div>
-                <img src="#" alt="">
+                <img src="${pokemonData.sprites.other.home.front_default}" alt="">
             </div>
-            <div>
-                <span>Type 1</span>
-                <span>Type 2</span>
+            <div id="pokemon-types${i}">
             </div>
         </div>
     `
 }
+
+function getPokemonTypesTemplate(pokemonData, i) {
+    return /*html*/`
+        <span>${pokemonData.types[i].type.name}</span>
+    `
+}
+
+getOnePokemonData();
