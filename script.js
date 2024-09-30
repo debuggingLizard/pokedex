33d62d04634c85d9ed189cfdd9369928c5235323
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-09-30 15:59:22 +0200

    modified design

--- script.js
+++ script.js
@@ -10,44 +10,43 @@ let url = "https://pokeapi.co/api/v2/";
 // getAllPokemonData();
 
 async function getOnePokemonData() {
-    for (let i = 1; i <= 20; i++) {
-        let response = await fetch(url + "pokemon/" + i);
-        let pokemonDataAsJson = await response.json();
-        console.log(pokemonDataAsJson);
-        renderPokemonEntries(pokemonDataAsJson, i);
-    }
+  for (let i = 1; i <= 20; i++) {
+    let response = await fetch(url + "pokemon/" + i);
+    let pokemonDataAsJson = await response.json();
+    console.log(pokemonDataAsJson);
+    renderPokemonEntries(pokemonDataAsJson, i);
+  }
 }
 
 function renderPokemonEntries(pokemonData, i) {
-    let entryRef = document.getElementById('main-content');
-    entryRef.innerHTML += getPokemonEntriesTemplate(pokemonData, i);
+  let entryRef = document.getElementById("main-content");
+  entryRef.innerHTML += getPokemonEntriesTemplate(pokemonData, i);
 
-    let typesRef = document.getElementById('pokemon-types' + i);
-    for (let i = 0; i < pokemonData.types.length; i++) {
-        typesRef.innerHTML += getPokemonTypesTemplate(pokemonData, i);
-    }
+  let typesRef = document.getElementById("pkm-types" + i);
+  for (let i = 0; i < pokemonData.types.length; i++) {
+    typesRef.innerHTML += getPokemonTypesTemplate(pokemonData, i);
+  }
 }
 
 function getPokemonEntriesTemplate(pokemonData, i) {
-    return /*html*/`
-        <div class="pokemon-entry">
-            <div>
-                <span>${pokemonData.name}</span>
-                <span>#${pokemonData.id}</span>
-            </div>
-            <div>
-                <img src="${pokemonData.sprites.other.home.front_default}" alt="">
-            </div>
-            <div id="pokemon-types${i}">
+  return /*html*/ `
+        <div class="pkm-entry">
+            <p class="pkm-entry-number">#${pokemonData.id}</p>
+            <div class="pkm-entry-info">
+                <div class="pkm-entry-info-left">
+                    <p>${pokemonData.name}</p>
+                    <div class="pkm-entry-types" id="pkm-types${i}"></div>
+                </div>
+                <img src="${pokemonData.sprites.other.home.front_default}" alt="Image of ${pokemonData.name}">
             </div>
         </div>
-    `
+    `;
 }
 
 function getPokemonTypesTemplate(pokemonData, i) {
-    return /*html*/`
+  return /*html*/ `
         <span>${pokemonData.types[i].type.name}</span>
-    `
+    `;
 }
 
 getOnePokemonData();
