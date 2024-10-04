4662150ed3b88d1e10ff4041a8ee25d14d067ec2
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-10-05 00:36:03 +0200

    modal design finished

--- script.js
+++ script.js
@@ -97,15 +97,15 @@ const modalTemplates = {
     return /*html*/ `
             <table>
               <tr>
-                <td>Height</td>
+                <td class="bold">Height</td>
                 <td>${modalData.height} dm</td>
               </tr>
               <tr>
-                <td>Weight</td>
+                <td class="bold">Weight</td>
                 <td>${modalData.weight} hg</td>
               </tr>
               <tr>
-                <td>Abilities</td>
+                <td class="bold">Abilities</td>
                 <td id="modal-pkm-abilities"></td>
               </tr>
             </table>
@@ -115,36 +115,37 @@ const modalTemplates = {
     return /*html*/ `
             <table>
               <tr>
-                <td>HP</td>
+                <td class="bold">HP</td>
                 <td>${modalData.stats[0].base_stat}</td>
               </tr>
               <tr>
-                <td>Attack</td>
+                <td class="bold">Attack</td>
                 <td>${modalData.stats[1].base_stat}</td>
               </tr>
               <tr>
-                <td>Defense</td>
+                <td class="bold">Defense</td>
                 <td>${modalData.stats[2].base_stat}</td>
               </tr>
               <tr>
-                <td>Special Attack</td>
+                <td class="bold">Special Attack</td>
                 <td>${modalData.stats[3].base_stat}</td>
               </tr>
               <tr>
-                <td>Special Defense</td>
+                <td class="bold">Special Defense</td>
                 <td>${modalData.stats[4].base_stat}</td>
               </tr>
               <tr>
-                <td>Speed</td>
+                <td class="bold">Speed</td>
                 <td>${modalData.stats[5].base_stat}</td>
-              </tr>
-              <tr>
-                <td>Total</td>
-                <td>...</td>
               </tr>
             </table>
     `;
   },
+  templateShiny: function (modalData) {
+    return /*html*/`
+      <img src="${modalData.sprites.other.home.front_shiny}" alt="Image of ${modalData.name}">
+    `
+  }
 };
 
 function closeModal() {
@@ -236,11 +237,11 @@ function getModalTemplate(modalData, index) {
           <div id="modal-top" class="${modalData.types[0].type.name}">
             <div class="modal-top-info">
                 <div class="modal-top-info-left">
-                <p id="modal-pkm-name">${modalData.name}</p>
+                <p id="modal-pkm-name">${(modalData.name).toUpperCase()}</p>
                 <p id="modal-pkm-types"></p>
               </div>
               <div class="modal-top-info-right">
-                <p>#${modalData.id}</p>
+                <p>#${(modalData.id).toString().padStart(3, '0')}</p>
               </div>
             </div>
             <img src="${
@@ -249,18 +250,15 @@ function getModalTemplate(modalData, index) {
           </div>
           <div class="modal-bottom">
             <div class="modal-nav">
-              <button onclick="renderModalStats('About', ${index})">About</button>
-              <button onclick="renderModalStats('BaseStats', ${index})">Base Stats</button>
+              <button id="btnAbout" class="focus" onclick="renderModalStats('About', ${index}); updateFocus('btnAbout')">ABOUT</button>
+              <button id="btnBase" onclick="renderModalStats('BaseStats', ${index}); updateFocus('btnBase')">BASE STATS</button>
+              <button id="btnShiny" onclick="renderModalStats('Shiny', ${index}); updateFocus('btnShiny')">SHINY</button>
             </div>
             <div id="modal-stats"></div>
           </div>
-          <div>
-            <button id="btn-prev" onclick="showModal(${
-              index - 1
-            })">previous pkm</button>
-            <button id="btn-next" onclick="showModal(${
-              index + 1
-            })">next pkm</button>
+          <div class="modal-buttons">
+            <button id="btn-prev" onclick="showModal(${index - 1})">&#8617; #${(index - 1).toString().padStart(3, '0')}</button>
+            <button id="btn-next" onclick="showModal(${index + 1})">#${(index + 1).toString().padStart(3, '0')} &#8618;</button>
           </div>
   `;
 }
@@ -276,6 +274,14 @@ async function renderModalStats(identifier, index) {
   if (identifier == "About") {
     renderModalAbilities(modalData);
   }
+}
+
+function updateFocus(button) {
+  document.getElementById('btnAbout').classList.remove('focus');
+  document.getElementById('btnBase').classList.remove('focus');
+  document.getElementById('btnShiny').classList.remove('focus');
+
+  document.getElementById(button).classList.add('focus');
 }
 
 // SEARCH FUNCTIONALITY
