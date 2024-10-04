2a93a4aea418c733b17347ee82b9b03dcef4d0b4
Lisa-Marie Maliga <lisa.maliga@outlook.com>
2024-10-04 13:50:37 +0200

    added loading animation

--- script.js
+++ script.js
@@ -12,19 +12,43 @@ function init() {
 // fetch each Pokemon-Data and execute render-body-Function
 async function getOnePokemonData() {
   let loadRef = document.getElementById("load-more-btn");
+  let loadingAnimation = document.getElementById('loading-animation');
+  let mainContentContainer = document.getElementById("main-content-container");
+
   loadRef.classList.add("d-none");
+  loadingAnimation.classList.remove('d-none');
+
+  let newEntries = [];
+  let pokemonDataArray = [];
+
   for (let i = 0; i < 20; i++) {
     if (indexStart == 152) {
       break;
     }
     let response = await fetch(url + "pokemon/" + indexStart);
     let pokemonDataAsJson = await response.json();
-    renderPokemonEntries(pokemonDataAsJson, indexStart);
+    newEntries.push(getPokemonEntriesTemplate(pokemonDataAsJson, indexStart));
+    pokemonDataArray.push(pokemonDataAsJson);
     indexStart++;
   }
+
+  newEntries.forEach((entry, index) => {
+    mainContentContainer.innerHTML += entry;
+    let typesRef = document.getElementById("pkm-types" + (indexStart - 20 + index));
+    let pokemonData = pokemonDataArray[index];
+    for (let j = 0; j < pokemonData.types.length; j++) {
+      typesRef.innerHTML += getPokemonTypesTemplate(pokemonData, j);
+    }
+  });
+
+  loadingAnimation.classList.add('d-none');
+
   if (indexStart < 152) {
     loadRef.classList.remove("d-none");
   }
+
+  newEntries = [];
+  pokemonDataArray = [];
 }
 
 async function getAllPokemonData() {
